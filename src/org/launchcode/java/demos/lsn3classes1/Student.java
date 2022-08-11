263c40dd9c2e5333cc776138cc7cbe9cf36cf4a8
Hope <hope_chambers@comcast.com>
2022-08-11 14:09:19 -0400

    class exercise practice

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -10,4 +10,26 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+
+    public void setName(String name){
+    this.name=name;
+}
+    private void setStudentId(Integer studentId){
+    this.studentId=studentId;
+}
+    public void setGpa(Double gpa){
+        this.gpa=gpa;
+    }
+    public void setNumberOfCredits(Integer numberOfCredits){
+    this.numberOfCredits=numberOfCredits;
+}
+    public String getName(){
+        return getName();
+    }
+    public Integer getStudentID(){
+        return getStudentID();
+    }
+    public Integer numberOfCredits(){
+        return getNumberOfCredits();
+    }
 }
