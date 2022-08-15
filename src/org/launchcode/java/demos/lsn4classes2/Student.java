724f0526fd0731956316cadac0ab623ea1ecf91a
Hope <hope_chambers@comcast.com>
2022-08-15 13:48:06 -0400

    Exercise from chapter 5 started

--- src/org/launchcode/java/demos/lsn4classes2/Student.java
+++ src/org/launchcode/java/demos/lsn4classes2/Student.java
@@ -30,9 +30,19 @@ public class Student {
 
 
      //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
-//        // Determine the grade level of the student based on numberOfCredits
-//    }
+         public static String getGradeLevel(int credits) {
+        if(credits <= 29){
+            return"freshman";
+        }else if (credits <= 59) {
+            return "sophomore";
+        }else if (credits <=89) {
+            return "Junior";
+        }else{
+            return "Senior";
+        }
+
+      // Determine the grade level of the student based on numberOfCredits
+    }
 
     // TODO: Complete the addGrade method.
     public void addGrade(int courseCredits, double grade) {
